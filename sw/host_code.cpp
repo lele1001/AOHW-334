2d1672ae74d3a9623f8cde7977f492dd7d53e721
lelele1001 <eleonora.cabai01@gmail.com>
2024-06-22 10:07:34 +0200

    added sw implementation

---
+++ sw/host_code.cpp
@@ -0,0 +1,278 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string>
+#include "experimental/xrt_kernel.h"
+#include "experimental/xrt_uuid.h"
+#include "../common/common.h"
+#include "math.h"
+#include "../aie/src/point.h"
+#include "../aie/src/cluster.h"
+
+// For hw emulation, run in sw directory: source ./setup_emu.sh -s on
+#define DEVICE_ID 0
+
+// every top function input that must be passed from the host to the kernel must have a unique index starting from 0
+// args indexes for setup_aie kernel
+#define arg_setup_aie_num_clusters 0
+#define arg_setup_aie_num_points 1
+#define arg_setup_aie_input 2
+
+// args indexes for sink_from_aie kernel
+#define arg_sink_from_aie_output 1
+#define arg_sink_from_aie_size 2
+
+// define the number of points and clusters
+#define NUM_CLUSTERS 2
+#define NUM_POINTS 10
+
+bool get_xclbin_path(std::string& xclbin_file);
+std::ostream& bold_on(std::ostream& os);
+std::ostream& bold_off(std::ostream& os);
+
+// Print the output
+void printOutput(int *output) {
+    int i;
+
+    for (i = 0; i < NUM_CLUSTERS * 2; i++)
+    {
+        if (i % 2 == 0)
+        {
+            std::cout << "Cluster (" << output[i] << ", ";
+        }
+        else
+        {
+            std::cout << output[i] << ") ";
+        }
+    }
+
+    std::cout << std::endl;
+}
+
+// Check the results
+int checkResult(int* input, int* output)
+{
+    int points[NUM_POINTS * 2];
+    int clusters[NUM_CLUSTERS * 2];
+    int clusters_accum[NUM_CLUSTERS * 2] = {0};
+    int cluster_points[NUM_CLUSTERS] = {0};
+
+    int i, j, p_idx, c_idx;
+    int x_diff, y_diff, cluster_num, cluster_index;
+    int min_distance;
+
+    // Initialize points and clusters
+    for (i = 0; i < NUM_POINTS * 2; i++)
+    {
+        points[i] = input[i];
+    }
+
+    for (i = 0; i < NUM_CLUSTERS * 2; i++)
+    {
+        c_idx = NUM_POINTS * 2 + i;
+        clusters[i] = input[c_idx];
+    }
+
+    // K-Means algorithm
+    for (i = 0; i < NUM_POINTS; i++)
+    {
+        int distances[NUM_CLUSTERS] = {0};
+        p_idx = i * 2;
+
+        // Calculate the distance between the point and each cluster
+        for (j = 0; j < NUM_CLUSTERS; j++)
+        {
+            c_idx = j * 2;
+            x_diff = clusters[c_idx] - points[p_idx];
+            y_diff = clusters[c_idx + 1] - points[p_idx + 1];
+            
+            distances[j] = x_diff * x_diff + y_diff * y_diff;
+            std::cout << "Distance between point (" << points[p_idx] << ", " << points[p_idx + 1] << ") and cluster (" << clusters[c_idx] << ", " << clusters[c_idx + 1] << ") is " << distances[j] << std::endl;
+        }
+
+        // Assign the point to the nearest cluster
+        cluster_num = -1;
+        min_distance = INT32_MAX;
+
+        for (j = 0; j < NUM_CLUSTERS; j++)
+        {
+            if (distances[j] < min_distance)
+            {
+                min_distance = distances[j];
+                cluster_num = j;
+            }
+        }
+
+        cluster_index = cluster_num * 2;
+
+        // Update the cluster coordinates
+        cluster_points[cluster_num]++;
+        clusters_accum[cluster_index] += points[p_idx];
+        clusters_accum[cluster_index + 1] += points[p_idx + 1];
+        // std::cout << "Accumulated coordinates for cluster " << cluster_num << " are (" << clusters_accum[cluster_index] << ", " << clusters_accum[cluster_index + 1] << ") with " << cluster_points[cluster_num] << " points" << std::endl;
+
+        if (cluster_points[cluster_index] > 0)
+        {
+            clusters[cluster_index] = clusters_accum[cluster_index] / cluster_points[cluster_num];
+            clusters[cluster_index + 1] = clusters_accum[cluster_index + 1] / cluster_points[cluster_num];
+            std::cout << "Cluster " << cluster_num << " coordinates updated to (" << clusters[cluster_index] << ", " << clusters[cluster_index + 1] << ")" << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << "Expected results:";
+    printOutput(clusters);    
+
+    // Compare the expected results with the output
+    for (i = 0; i < NUM_CLUSTERS * 2; i++) 
+    {
+        if (clusters[i] != output[i]) 
+        {
+            std::cout << "Error at index " << i << ": " << clusters[i] << " != " << output[i] << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+
+    std::cout << "Test passed!" << std::endl;
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+    int input_size = (NUM_CLUSTERS + NUM_POINTS) * 2;
+    int output_size = NUM_CLUSTERS * 2;
+    int nums[input_size];
+    std::srand(time(nullptr));
+
+    int i, j, random_num;
+
+    // Generate random coordinates for points and clusters
+    for (i = 0; i < input_size; i++)
+    {
+        random_num = std::rand() % 20 - 10;
+        nums[i] = random_num;
+
+        if (i % 2 == 0)
+        {
+            if (i < NUM_POINTS * 2)
+            {
+                std::cout << "Point (" << random_num << ", ";
+            }
+            else
+            {
+                std::cout << "Cluster (" << random_num << ", ";
+            }
+        }
+        else
+        {
+            std::cout << random_num << ") ";
+        }
+    }
+
+    std::cout << std::endl;
+
+//------------------------------------------------LOADING XCLBIN------------------------------------------    
+    std::string xclbin_file;
+    if (!get_xclbin_path(xclbin_file))
+    {
+        return EXIT_FAILURE;
+    }
+
+    // Load xclbin
+    std::cout << "1. Loading bitstream (" << xclbin_file << ")... ";
+    xrt::device device = xrt::device(DEVICE_ID);
+    xrt::uuid xclbin_uuid = device.load_xclbin(xclbin_file);
+    std::cout << "Done" << std::endl;
+//----------------------------------------------INITIALIZING THE BOARD------------------------------------------
+
+    // create kernel objects
+    xrt::kernel krnl_setup_aie  = xrt::kernel(device, xclbin_uuid, "setup_aie");
+    xrt::kernel krnl_sink_from_aie  = xrt::kernel(device, xclbin_uuid, "sink_from_aie");
+
+    // get memory bank groups for device buffer - required for axi master input/ouput
+    xrtMemoryGroup bank_output  = krnl_sink_from_aie.group_id(arg_sink_from_aie_output);
+    xrtMemoryGroup bank_input  = krnl_setup_aie.group_id(arg_setup_aie_input);
+
+    // create device buffers - if you have to load some data, here they are
+    xrt::bo buffer_setup_aie = xrt::bo(device, input_size * sizeof(int32_t), xrt::bo::flags::normal, bank_input);
+    xrt::bo buffer_sink_from_aie = xrt::bo(device, output_size * sizeof(int32_t), xrt::bo::flags::normal, bank_output); 
+
+    // create runner instances
+    xrt::run run_setup_aie   = xrt::run(krnl_setup_aie);
+    xrt::run run_sink_from_aie = xrt::run(krnl_sink_from_aie);
+
+    // set setup_aie kernel arguments
+    run_setup_aie.set_arg(arg_setup_aie_num_clusters, NUM_CLUSTERS);
+    run_setup_aie.set_arg(arg_setup_aie_num_points, NUM_POINTS);
+    run_setup_aie.set_arg(arg_setup_aie_input, buffer_setup_aie);
+
+    // set sink_from_aie kernel arguments
+    run_sink_from_aie.set_arg(arg_sink_from_aie_output, buffer_sink_from_aie);
+    run_sink_from_aie.set_arg(arg_sink_from_aie_size, output_size);
+
+    // write data into the input buffer
+    buffer_setup_aie.write(nums);
+    buffer_setup_aie.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+    // run the kernel
+    run_sink_from_aie.start();
+    run_setup_aie.start();
+
+    // wait for the kernel to finish
+    run_setup_aie.wait();
+    run_sink_from_aie.wait();
+
+    // read the output buffer
+    buffer_sink_from_aie.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    int output_buffer[output_size];
+    buffer_sink_from_aie.read(output_buffer);
+
+    // print the output
+    std::cout << "Output: ";
+    printOutput(output_buffer);
+
+    std::cout << std::endl; 
+    
+// ------------------------------------------------CHECKING THE RESULTS------------------------------------------  
+    return checkResult(nums, output_buffer);
+}
+
+
+bool get_xclbin_path(std::string& xclbin_file) {
+    // Judge emulation mode accoring to env variable
+    char *env_emu;
+    if (env_emu = getenv("XCL_EMULATION_MODE")) {
+        std::string mode(env_emu);
+        
+        if (mode == "hw_emu")
+        {
+            std::cout << "Program running in hardware emulation mode" << std::endl;
+            xclbin_file = "overlay_hw_emu.xclbin";
+        }
+        else
+        {
+            std::cout << "[ERROR] Unsupported Emulation Mode: " << mode << std::endl;
+            return false;
+        }
+    }
+    else {
+        std::cout << bold_on << "Program running in hardware mode" << bold_off << std::endl;
+        xclbin_file = "overlay_hw.xclbin";
+    }
+
+    std::cout << std::endl << std::endl;
+    return true;
+}
+
+std::ostream& bold_on(std::ostream& os)
+{
+    return os << "\e[1m";
+}
+
+std::ostream& bold_off(std::ostream& os)
+{
+    return os << "\e[0m";
+}
+
